eede852f70670b440b847e021d61358eed5073d6
finnc <20106642@mail.wit.ie>
2026-09-14 13:01:01 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,9 +1,37 @@
-import { friends } from './01-basics'
+import { colleagues, friends } from './01-basics'
 import {Friend, Colleague } from './myTypes'
 
 function older(f: Friend) : string {
      f.age += 1
      return `${f.name} is now ${f.age}` 
 }
-
 console.log(older(friends[0]))
+
+//   -------------------
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+console.log(highestExtension(colleagues.current));
+
+function addColleague(c: Colleague[], name: string, department: string, email: string): string {
+    const extension = highestExtension(colleagues.current).contact.extension + 1;
+
+    const colleague: Colleague = {
+        name,
+        department,
+        contact: {
+            email,
+            extension
+        }
+    };
+
+    colleagues.current.push(colleague);
+    return `${name} has been added to the colleagues list with extension ${extension}.`;
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
